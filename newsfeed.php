6696d1f20421c708ff8d5059c43a58d702070fc2
BercoviciAdrian-2A6 <adrianbercovici.work@gmail.com>
2023-06-12 23:54:16 +0300

    added sql tables

--- newsfeed.php
+++ newsfeed.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
-include("connections.php");
-include("functions.php");
-$user_data = check_login($con);
+#session_start();
+#include("connections.php");
+#include("functions.php");
+#$user_data = check_login($con);
 ?>
 <!DOCTYPE html>
 <html lang="en">
