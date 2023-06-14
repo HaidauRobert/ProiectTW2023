10ce42415ea06d6f69ca6d65c6af36cf28d97de9
BercoviciAdrian-2A6 <adrianbercovici.work@gmail.com>
2023-06-14 17:42:56 +0300

    Connection to db + sql table create commands

---
+++ MVC/app/core/Database.php
@@ -0,0 +1,8 @@
+<?php
+$db_server_name = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "web2023";
+
+$connection = mysqli_connect($db_server_name, $db_username, $db_password, $db_name);
+
