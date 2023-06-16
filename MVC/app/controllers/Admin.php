e31df7edf625f788cc8e8d70defdc303670accdc
Robert <robert@192-168-0-100.rdsnet.ro>
2023-06-16 16:50:08 +0300

    same as before

--- MVC/app/controllers/Admin.php
+++ MVC/app/controllers/Admin.php
@@ -1,15 +1,55 @@
 <?php
-
+require_once __DIR__ . '/../models/AdminModel.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 class Admin extends Controller {
     public function index() {
         echo "This is the controller of the admin page.";
 
         $m = new Model;
-        if(check_login($m->connection) == 'Failed') {
-            $this->view('login');
+        check_login($m->connection);
+        $adminModel = new AdminModel();
+        $data = array();
+        $data['tables'] = $adminModel->getTables(); 
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['addTag'])) {
+                $tagName = $_POST['tagName'];
+                $result = $adminModel->addTag($tagName);
+                if ($result === 'success') {
+                    $data['addTagMessage'] = 'Successfully added.';
+                } elseif ($result === 'exists') {
+                    $data['addTagMessage'] = 'Tag already exists in the database.';
+                } else {
+                    $data['addTagMessage'] = 'Failed to add tag.';
+                }
+            } elseif (isset($_POST['removeTag'])) {
+                $tagName = $_POST['tagName'];
+                $result = $adminModel->removeTag($tagName);
+                if ($result === 'success') {
+                    $data['removeTagMessage'] = 'Successfully removed.';
+                } elseif ($result === 'not_found') {
+                    $data['removeTagMessage'] = 'Tag does not exist in the database.';
+                } else {
+                    $data['removeTagMessage'] = 'Failed to remove tag.';
+                }
+            } elseif (isset($_POST['databaseSelect'], $_POST['formatSelect'])) {
+                $database = $_POST['databaseSelect'];
+                $format = $_POST['formatSelect'];
+            
+                if ($format === 'csv') {
+                    $tableName = $_POST['databaseSelect']; 
+                    $adminModel->exportTableAsCSV($tableName);
+                    $data['csvDone'] = __DIR__ . '/../../public/csv/' . $tableName. '.csv';
+                } else {
+                    $tableName = $_POST['databaseSelect'];
+                    $adminModel->exportTableAsJSON($tableName);
+                    $data['jsonDone'] = __DIR__ . '/../../public/csv/' . $tableName. '.csv';
+                }
+            }
+            
         }
-        else {
-            $this->view('admin');
-        }
+
+        $this->view('admin', $data);
     }
 }
--- MVC/app/views/admin.view.php
+++ MVC/app/views/admin.view.php
@@ -21,23 +21,41 @@
             <b><h1 class="panel-text">Admin Panel</h1></b>
             <div class="tag-buttons">
                 <h3>Write the name of a tag in a box and click 'Add' to add that tag, or 'Remove' to remove it</h3>
-                <input type="text" id="tagName" placeholder="Enter tag name">
-                <button class="button" id="addTag">Add</button>
-                <button class="button" id="removeTag">Remove</button>
+                <form method="post">
+                    <input type="text" name="tagName" id="tagName" placeholder="Enter tag name">
+                    <button class="button" type="submit" name="addTag" id="addTag">Add</button>
+                    <button class="button" type="submit" name="removeTag" id="removeTag">Remove</button>
+                </form>
+                    <?php if (!empty($data['addTagMessage'])): ?>
+                        <p><?php echo $data['addTagMessage']; ?></p>
+                    <?php endif; ?>
+                    <?php if (!empty($data['removeTagMessage'])): ?>
+                        <p><?php echo $data['removeTagMessage']; ?></p>
+                    <?php endif; ?>
             </div>
             <div class="export-data">
-                <h3>Select the database you want to export, then the format you want it exported in and finally click the Export button to download it</h3>
-                <select id="databaseSelect">
-                    <option value="" disabled selected>Select database</option>
-                    <option value="database1">Database 1</option>
-                    <option value="database2">Database 2</option>
+                <h3>Select the table you want to export, then choose the format and click the Export button to download it</h3>
+                <form action="" method="POST">
+                <select id="databaseSelect" name="databaseSelect">
+                    <option value="" disabled selected>Select table</option>
+                    <?php foreach ($data['tables'] as $table) : ?>
+                        <option value="<?php echo $table; ?>"><?php echo $table; ?></option>
+                    <?php endforeach; ?>
                 </select>
-                <select id="formatSelect">
+                <select id="formatSelect" name="formatSelect">
                     <option value="" disabled selected>Select format</option>
                     <option value="csv">CSV</option>
                     <option value="json">JSON</option>
                 </select>
-                <button class="button" id="exportButton">Export</button>
+                <button class="button" id="exportButton" name="exportButton">Export</button>
+                </form>
+                <?php if (!empty($data['csvDone'])): ?>
+                    <p>Tabelul a fost exportat cu succes in folderul "csv" din public</p>
+                <?php endif; ?>
+                <?php if (!empty($data['jsonDone'])): ?>
+                    <p>Tabelul a fost exportat cu succes in folderul "json" din public</p>
+                <?php endif; ?>
             </div>
+
 	</div>
 </body>
