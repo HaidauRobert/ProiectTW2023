5853e52d69349d2ba2892b346b3f2b3525939127
BercoviciAdrian-2A6 <adrianbercovici.work@gmail.com>
2023-06-16 17:29:04 +0300

    newsfeed controller in progress

--- MVC/app/controllers/Newsfeed.php
+++ MVC/app/controllers/Newsfeed.php
@@ -1,15 +1,24 @@
 <?php
 
+require_once __DIR__ . '/../models/NewsfeedModel.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 class Newsfeed extends Controller {
     public function index() {
         echo "This is the controller of the NewsFeed page.";
 
-        $m = new Model;
-        if(check_login($m->connection) == 'Failed') {
-            $this->view('login');
-        }
-        else {
-            $this->view('newsfeed');
-        }
+        $data = array();
+
+        $model = new NewsfeedModel();
+
+        $userId = $_SESSION['userid'];
+        check_login($model->connection);
+
+        $data['user_subscriptions'] = $model->get_user_subscriptions($userId);
+
+        $data['newsfeed_posts'] = $model->get_newsfeed_entries($userId);
+
+        $this->view('newsfeed');
     }
 }
